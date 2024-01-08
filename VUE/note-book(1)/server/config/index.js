0c68a4c764b8b1968e5bf8f960a06b36de85fa6b
初心丨胡先生 <hcjforgeahead@qq.com>
2024-01-08 21:37:49 +0800

    note-book系统

---
+++ VUE/note-book(1)/server/config/index.js
@@ -0,0 +1,11 @@
+const config = {
+  database: {
+    DATABASE: 'noteBook',
+    USERNAME: 'root',
+    PASSWORD: '12345678',
+    PORT: '3306',
+    HOST: 'localhost'
+  }
+}
+
+module.exports = config
